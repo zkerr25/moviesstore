1023a3eb0a96f2e0033c4d3fd1c376089fd332dd
Zachary Kerrish <zkerrish@gmail.com>
2025-09-06 17:02:52 -0400

    second version

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'moviesstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'moviesstore',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
